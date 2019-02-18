934e5796d16896145514fd4717d2f75df2365db9
rnbehm <behmrachel@yahoo.com>
2019-02-17 19:40:23 -0800

    updated the summary tab to give context to the purpose of the app. still needs more specific app info

--- RachelBehm/app.R
+++ RachelBehm/app.R
@@ -41,15 +41,19 @@ ui<- fluidPage(
   titlePanel("Exploring the UCSB Invertebrate Zoology Collection"),
   navbarPage("UCSB IZC",
              #this tab only has text
-             tabPanel("Summary of the App",
-                      h1("The purpose of this App"),
-                      h2("Uses of this app"),
-                      p("paragraph 1"),
-                      p("paragraph 2"),
-                      h1("Then another header"),
-                      p("paragraph3")
+             tabPanel("Summary",
+                      h1("History of the Invertebrate Zoology Collection"),
+                      h4("Introduction"),
+                      p("The University of California, Santa Barbara (UCSB) Natural History Museum at the Cheadle Center for Biodiversity and Ecological Restoration (CCBER) has formed an Invertebrate Zoology Collection from 10,000 specimens rediscovered in a basement on campus. Since its discovery, this collection hasgrown rapidly through coastal California arthropod survey efforts, donated student collections,and faculty research projects.These surveys, conducted by CCBER for conservation and restoration monitoring, are hugely valuable as the coastal regions of Santa Barbara and Ventura County are critically endangered habitats, withover 95% of these areas lost to human disturbance, and online records about insects from these areas is presently uncommon."),
+                      p("The creation of this collection has inspired new interest in entomology on campus. Undergraduate students, graduate students, and staff are learning basic entomology in newly formed classes and workshops, students are using the collection as a reference, and the inclusion of arthropods in faculty research is on the rise. The collection is providing space to voucher invertebrate research from UC Santa Barbara, and it has additional specimens and online data from our survey traps for researchers interested in California invertebrates"),
+                      h4("Background / Revitilazation Process"),
+                      p("The Invertebrate Zoology Collection originated as a teaching collection from an entomology class taught by Dr. Adrian Wenner in the 1950's. The collection was expanded to include specimens collected by Dr. Wenner in the 1960's. After he retired during the 1980's, the collection was abandoned. In 2015, it was rediscovered and incorporated into the museum at the CCBER. The collection began to grow again from student-donated specimens from the presentday UCSB entomology and invertebrate zoology courses, accessions from the UCSB Natural Reserve System, and regional arthropod survey projects"),
+                      p("The historic collection had out-of-date identifications, if any, and were coarsely organized by family. Thanks to funding from the Institute of Museum and Library Services (IMLS) and the UCSB Coastal Fund, new drawers and unit trays were purchased,  nomenclature was coarsely updated, and specimen determinations re-examined. Specimen are determined to the lowest rank possible with a focus on bees, ants, tiger beetles, and dune insects."),
+                      p("Starting in April 2017, databasing the specimens began. Specimens were given a barcode that acts as a unique identifier for the specimen. Once imaged, the photo is colorcorrected, cropped, and renamed using a custom Gimp python plugin called BugFlipper, and bulk-uploaded into our Symbiota data portal."),
+                      h1("Purpose of this App")
+                      ),
                       
-             ),
+             
              #histogram panel- like what you wanna do 
              #the histogram is made in the server code and called in this code
              tabPanel("Exploration",
